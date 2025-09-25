3ea456b87eb540ad8eb6f9062c3d4aea88fe76a2
luannisilva <luanni.gabriely.silva@gmail.com>
2025-09-24 21:57:20 -0300

    adicionei a aula 6

---
+++ Aulas/Aula6/script.js
@@ -0,0 +1,44 @@
+ /*let a = 10
+let b = 10
+console.log(b)
+b = 5
+console.log(a,b)  */
+// o let esta guardando uma variavel number  e o console.log(b) vai imprimir  o numero 10  e o console.log(a,b)
+// imprimir 10,5
+
+ /* let a = 10
+ let b = 20
+ c = a
+ b = c
+ a = b
+ console.log( a,b,c) */
+ // o let esta guardando as variaveis e o console.log(a,b,c) vai imprimir 10,10,10
+
+
+  /* let horarioDeServico = prompt ("Quantas horas voce trabalha por dia")
+  let valorSalarialPorDia = prompt ("Quanto voce recebe por dia")
+  console.log("eu recebe","R$",valorSalarialPorDia, "por dia  e trabalho ",horarioDeServico," horas por dia ")
+  /* esses prontms armazenam as variaveis horarioDeServiço e valorSalarialPorDia o horarioDeServiço 
+  faz uma pergunta sobre quantas horas voce trabalha por dia 
+   e o  valorSalarialPorDia pergunta sobre qunato voce recebe por dia 
+   o console.log("eu recebe",valorSalarialPorDia, "por dia  e trabalho ",horarioDeServiço,"por dia ") 
+    vai imprmir  eu recebo horarioDeServico por dia e trabalho valorSalarialPorDia   horas por dia  */
+
+
+    let  nome 
+    let idade 
+     console.log(typeof(nome),typeof(idade) ) 
+   /*    pq nao tem nada definido     */                    
+ nome = prompt ("qual seu nome ")
+ idade = prompt("qual sua idade" ) 
+ console.log("olá",nome,"voce tem",idade)  
+ console.log(typeof(nome),typeof(idade) )
+  /*  esses prontms armazenam as variaveis   nome e idade e
+  faz uma pergunta sobre qual seu nome
+     e qual sua idade
+   o console.log("olá",nome, "voce tem ",idade,) 
+    olá nome voce tem idade  */ 
+ 
+
+
+
