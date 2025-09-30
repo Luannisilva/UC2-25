266107a4045991fac4103422c0e31d8b999cb98f
luannisilva <luanni.gabriely.silva@gmail.com>
2025-09-29 21:42:51 -0300

    adicionei a aula 7

--- Aulas/Aula6/script.js
+++ Aulas/Aula6/script.js
@@ -25,20 +25,60 @@ console.log(a,b)  */
     vai imprmir  eu recebo horarioDeServico por dia e trabalho valorSalarialPorDia   horas por dia  */
 
 
-    let  nome 
+    /*     let  nome 
     let idade 
      console.log(typeof(nome),typeof(idade) ) 
    /*    pq nao tem nada definido     */                    
- nome = prompt ("qual seu nome ")
+  /*    nome = prompt ("qual seu nome ")
  idade = prompt("qual sua idade" ) 
  console.log("olá",nome,"voce tem",idade)  
- console.log(typeof(nome),typeof(idade) )
+ console.log(typeof(nome),typeof(idade) ) */
   /*  esses prontms armazenam as variaveis   nome e idade e
   faz uma pergunta sobre qual seu nome
      e qual sua idade
    o console.log("olá",nome, "voce tem ",idade,) 
     olá nome voce tem idade  */ 
  
+
+    let  blusa 
+    let altura 
+    let  comida
+
+    blusa = prompt("sua blusa é preta ")
+    altura = prompt("Sua altura é de 1,70")
+    comida = prompt("voce jantou hj ")
+ 
+     console.log( " sua blusa é preta ",blusa,",Sua altura é de 1,70",altura,",voce jantou hj",comida,)
+
+ let a = 10
+ let b = 25
+
+ // Aqui faremos ua logica para trocar os valores 
+ c=a
+ a=b
+ b=c
+
+ // Depois de trocados , teremos o seguinte resultado :
+ console.log(" o novo valor de a é",a) // O novo valor de a é 25
+ console.log("O  novo valor de b é ",b) // P novo valor de b é 10
+
+ let nuemro1 = prompt(8)
+ let numero2 = prompt(13)
+let soma = parseInt(8)+parseInt(13)
+console.log("a soma é",soma)
+
+
+
+
+
+
+
+
+
+
+
+
+     
 
 
 
---
+++ Aulas/Aula7/script.js
@@ -0,0 +1,21 @@
+ const primeiroNumero = 10
+ const segundoNumero = 20 
+
+ console.log(primeiroNumero+segundoNumero+4)
+
+
+console.log(primeiroNumero-segundoNumero)
+  const primeiroValor = 10
+  const segundoValor = 25
+
+ console.log(primeiroNumero*segundoNumero)
+
+ console.log(primeiroNumero/segundoNumero)
+
+ const nuemro1 = 345
+ const numero2 = 10
+ console.log(nuemro1 / numero2)
+
+ let idade = 17
+ idade= idade+1
+ console.log(idade)
