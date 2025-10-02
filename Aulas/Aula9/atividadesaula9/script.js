3658d9383381cfde2e3811265010f8039e2cb6c0
luannisilva <luanni.gabriely.silva@gmail.com>
2025-10-01 21:54:40 -0300

    adicionei a aula  aula 9

--- Aulas/Aula9/atividadesaula9/script.js
+++ Aulas/Aula9/atividadesaula9/script.js
@@ -32,4 +32,37 @@ console.log("seu nome é",nome ," voce  tem " ,idade," anos",
 " voce é  maior de idade", maiorDeIdade," em 2050 voce vai  ter",idadeEmDoisMilECinquenta)
 
 
+ const bool1 = true
+ const bool2 =  false
+ const boll3 = !bool2 // aqui vai ficar true
+
+ let resultado = bool1 && bool2
+ console.log("a",resultado)
+  resultado = bool1 && bool2 && boll3
+  console.log("b", resultado)
+  resultado = !resultado && (bool1 || bool2)
+  console.log("c",resultado)
+  console.log("d",typeof resultado)
+/* o console.log("a") vai imprimir  a,  false console.log("b") vai imprimir  b ,false 
+console.log("c") vai imprimir c, true console.log("d") vai imprimir d, boolean */  
+
+
+let primeiroNumero = prompt(" digite um numero !")
+let segundoNumero = prompt(" digite outro numero !")
+
+const soma = primeiroNumero = segundoNumero
+ console.log(soma)
+
+  /*o  prompt let primeiroNumero  vai perguntar um numero e o outro 
+ prompt do segundoNumero vai perguntar outro numero
+   o const soma vai  somar o primeiro numero e o segundo 
+    o console.log(soma) vai imprimir a soma dos o primeiro numero 
+    +  o segundo numero  */ 
+
+
+
+
+
+
+
 
