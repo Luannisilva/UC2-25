684dc5836506d7c8ef174127e677fcdb366553e9
luannisilva <luanni.gabriely.silva@gmail.com>
2025-10-07 21:50:25 -0300

    adicionei a aula  aulas 11

---
+++ Aulas/Aula11/atividadeaula11/script.js
@@ -0,0 +1,23 @@
+ /*const numeros = [1,2,3,4,5,6]
+ console.log(numeros.length)
+ numeros.push(7)
+ console.log(numeros)
+ numeros.splice(3,1,4,1)*/
+
+ let  array
+ console.log('a.',array)
+ array = null
+ console.log('b.',array)
+  array = [3,4,5,6,7,8,9,10,11,12,13]
+  console.log('c',array.length)
+  let i = 0 
+  console.log('d.',array[i])
+  array[i+1]= 19
+console.log('e.',array)
+const valor = array[i+6]
+console.log('f',valor)
+
+/* o console.log('a.',array) vai dar indefinida ,  console.log('b.',array) vai dar null ,
+ console.log('c',array.length) vai imprimir 11 , console.log('d.',array[i])   vai imprimir 3 ,
+ console.log('e.',array) vai imprimir 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,
+  console.log('f',valor) vai imprimir 9 */ 
---
+++ Aulas/Aula11/script.js
@@ -0,0 +1,26 @@
+const cachorros = ["Pastor-alemão","Husky-siberiano","Dobermann","Rottweiler","pinscher"]
+const segundoItem =  prompt("digite um numero de 0 a 4")
+console.log(cachorros[0])
+ const pokemon = ["bulbasaur","squirtle","charmande"]
+console.log(pokemon.length)
+
+const serrieBoa = ["viking, supernatural"]
+console.log(serrieBoa.includes("vikings"), serrieBoa.includes("GOT Of Gamer Trhones"))
+
+const numeros = [1,2,3]
+numeros.push(4)
+console.log(numeros)
+
+numeros.push(5,6,7)
+console.log(numeros)
+
+const meusPeixes = [" palhaço","mandarin","esturjão"]
+meusPeixes.pop()
+console.log(meusPeixes)
+
+ const letras =["A","B","C","D","E","F","G","H"]
+ letras.splice(2,1)
+
+ letras.splice(3,2)
+ 
+ console.log(letras)
