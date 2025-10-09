344deab3f3812ab4e017efec71145f4a6f2a0dc9
luannisilva <luanni.gabriely.silva@gmail.com>
2025-10-08 21:49:35 -0300

    adicionei a aula  aulas 11 e desafio

--- Aulas/Aula11/atividadeaula11/script.js
+++ Aulas/Aula11/atividadeaula11/script.js
@@ -21,3 +21,39 @@ console.log('f',valor)
  console.log('c',array.length) vai imprimir 11 , console.log('d.',array[i])   vai imprimir 3 ,
  console.log('e.',array) vai imprimir 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,
   console.log('f',valor) vai imprimir 9 */ 
+ 
+  const frase = prompt("digite uma frase")
+   console.log(frase.toUpperCase().replaceAll("a","i"),frase.length)
+   /* "Subi num ônibus em Marrocos"? */
+        /*  o const frase  vai perguntar pra vc digitar uma frase 
+        ja o  console.log(frase.toUpperCase().replaceAll("a","i"),frase.length) vai imprimir  SUBI NUM ÔNIBUS EM MARROCOS ?  29  */
+
+        emailDoUsuario = prompt("escreva seu  e-mail") 
+        nomeDoUsuario = prompt("qual seu nome")
+ console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o),
+ ${nomeDoUsuario}`)
+
+  const minhasComidas = [ "Estrogonofe","pizza","acai","xis","melancia"]
+  console.log(minhasComidas)
+  console.log("Essas são as minhas comidas preferidas")
+   console.log(minhasComidas[0])
+  console.log(minhasComidas[1])
+  console.log(minhasComidas[2])
+  console.log(minhasComidas[3])
+  console.log(minhasComidas[4])
+  comidaDoUsuario = prompt("escreva sua comida favorita") 
+  console.log(minhasComidas[3]=comidaDoUsuario)
+  console.log(minhasComidas)
+
+let listaDeTarefas = []
+const tarefaUsuario1  = prompt("qual sua 1 atividade do seu dia")
+novaTarefa1 = tarefaUsuario1
+const tarefaUsuario2  = prompt("qual sua 2 atividade do seu dia")
+novaTarefa2 = tarefaUsuario2
+const tarefaUsuario3  = prompt("qual sua 1 atividade do seu dia")
+novaTarefa3 = tarefaUsuario3
+console.log(listaDeTarefas[0]= novaTarefa1,listaDeTarefas[1],novaTarefa2,listaDeTarefas[2]=novaTarefa3)
+console.log(listaDeTarefas)
+const indice = prompt("qual indice da tarefa realizada")
+console.log(listaDeTarefas.splice(indice,1))
+console.log(listaDeTarefas)
---
+++ Aulas/Aula11/desafio/script.js
@@ -0,0 +1,11 @@
+const frase = prompt("escreva uma frase")
+const palavras = frase.split("")
+console.log(palavras)
+
+const frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+const indiceAbacaxi = frutas.indexOf("Abacaxi")
+
+const tamanhoArray = frutas.length
+
+console.log("Índice de 'Abacaxi':", indiceAbacaxi)
+console.log("Tamanho do array:", tamanhoArray)   
